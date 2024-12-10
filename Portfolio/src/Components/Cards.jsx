e01af61b6181feb6f88acb3dc504bbf7936d2724
siddique <siddiqueali531@gmail.com>
2024-12-10 16:57:53 +0500

    done with conflicts

---
+++ Portfolio/src/Components/Cards.jsx
@@ -0,0 +1,20 @@
+
+import CardItem from './CardItem';
+import imge1 from "../assets/gondol.png"
+import imge2 from "../assets/lights.png"
+import imge3 from "../assets/logo2.png"
+import imge4 from "../assets/me2.png"
+import imge5 from "../assets/nature.png"
+import imge6 from "../assets/p1.png"
+export default function Cards() {
+  return (
+    <div className='bg-slate-100 grid grid-cols-3 gap-7 p-9'>
+      <CardItem imge={imge1} />
+      <CardItem imge={imge2} />
+      <CardItem imge={imge3} />
+      <CardItem imge={imge4} />
+      <CardItem imge={imge5} />
+      <CardItem imge={imge6} />
+    </div>
+  )
+}
