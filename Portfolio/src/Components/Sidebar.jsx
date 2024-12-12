bdadfcd0668c57d235794402f8d89fe05b574491
siddique <siddiqueali531@gmail.com>
2024-12-12 20:35:35 +0500

    scroll done

--- Portfolio/src/Components/Sidebar.jsx
+++ Portfolio/src/Components/Sidebar.jsx
@@ -9,26 +9,38 @@ import Header from "./Header";
 export default function Sidebar() {
   
   return (
-    <div className={`lg:min-w-16 flex flex-col gap-5 pt-5 pl-4 h-screen w-[240px] `}>
+    <div className={` flex flex-col gap-5 pt-5 pl-4 h-screen w-[240px] `}>
       <div className="flex flex-col items-start gap-6 ">
         <img className="rounded-lg h-28" src={logo}  />
-        <h1 className="text-xl">PORTFOLIO</h1>
+        <h1 className="text-xl" >PORTFOLIO</h1>
       </div> 
       <div className="mt-8 w-full ">
         <ul className="flex flex-col gap-4  font-thin w-full">  
-          <NavLink to="#header">
-          <li className="hover:bg-slate-200 w-full px-2 cursor-pointer  py-2 flex items-center  gap-2 ">
+          <NavLink>
+          <li onClick={()=>window.scrollTo({
+  top: 0,
+  left: 0,
+  behavior: "smooth",
+})} className="hover:bg-slate-200 w-full px-2 cursor-pointer  py-2 flex items-center  gap-2 ">
           <span className="text-xl">< IoAppsSharp /></span>
           PORTFOLIO
           </li>
           </NavLink>
-          <NavLink to="./about">
-          <li className="hover:bg-slate-200 w-full px-2 cursor-pointer  py-2 flex items-center gap-2 ">
+          <NavLink>
+          <li  onClick={()=>window.scrollTo({
+  top: 1000,
+  left: 0,
+  behavior: "smooth",
+})} className="hover:bg-slate-200 w-full px-2 cursor-pointer  py-2 flex items-center gap-2 ">
           <span className="text-xl"><IoMdContact/></span>  ABOUT
           </li>
           </NavLink>
-          <NavLink to="./contact">
-          <li className="hover:bg-slate-200 w-full px-2 cursor-pointer  py-2 flex items-center gap-2">
+          <NavLink >
+          <li onClick={()=>window.scrollTo({
+  top: 2470,
+  left: 0,
+  behavior: "smooth",
+})} className="hover:bg-slate-200 w-full px-2 cursor-pointer  py-2 flex items-center gap-2">
           <span className="text-xl"><IoMail /></span>
           CONTACT
           </li>
